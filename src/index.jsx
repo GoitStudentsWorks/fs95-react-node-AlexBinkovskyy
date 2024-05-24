ab3572dfdf30a2e8de80caa786c3508f1c8e2936
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-05-24 23:59:50 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   //  <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/finalTeamProject">
+      <BrowserRouter basename="/fs95-react-node-AlexBinkovskyy">
         <ThemeProvider>
           <App />
           <ThemedToastContainer />
